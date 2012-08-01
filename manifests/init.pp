0005aa36bd6260e4e9b1187ed4466dda6b61f945
Kit Plummer <kitplummer@gmail.com>
2012-08-01 10:57:34 -0700

    adding missing wrapper libs

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class ddf($version = "enterprise-2.1.0.ALPHA3") {
 	package{ "postgresql-9.1-postgis": ensure => installed }
 
 	service { "postgres": ensure => running }
+
 	user { "ddf":
 		ensure => 'present'
 	}
@@ -33,7 +34,22 @@ class ddf($version = "enterprise-2.1.0.ALPHA3") {
 		source => "puppet:///modules/ddf/ddf",
 		require => File["/usr/local/ddf-${version}"],
 		mode => 755
-	} 
+	}
+	file { "/usr/local/ddf-${version}/lib/libwrapper.so":
+		source => "puppet:///modules/ddf/libwrapper.so",
+		require => File["/usr/local/ddf-${version}"],
+		mode => 644
+	}
+	file { "/usr/local/ddf-${version}/lib/karaf-wrapper.jar":
+		source => "puppet:///modules/ddf/karaf-wrapper.jar",
+		require => File["/usr/local/ddf-${version}"],
+		mode => 644
+	}
+	file { "/usr/local/ddf-${version}/lib/karaf-wrapper-main.jar":
+		source => "puppet:///modules/ddf/karaf-wrapper-main.jar",
+		require => File["/usr/local/ddf-${version}"],
+		mode => 644
+	}  
 	file { "/usr/local/ddf-${version}/bin/DDF-wrapper":
 		source => "puppet:///modules/ddf/DDF-wrapper",
 		mode => 755,
