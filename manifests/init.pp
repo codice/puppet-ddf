0db608b45284d65826a03d9b3312307fb986f2e4
Kit Plummer <kitplummer@gmail.com>
2012-08-02 07:36:24 -0700

    updating to support wget fetch from MACE

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,60 +1,77 @@
-class ddf($version = "enterprise-2.1.0.ALPHA3") {
+class ddf($package = "enterprise",
+		  $version = "2.1.0.ALPHA3") {
 
+	# Ensure system dependencies are installed
 	package{ "unzip": ensure => installed }
 	package{ "openjdk-6-jre-headless": ensure => installed }
 	package{ "postgresql-9.1-postgis": ensure => installed }
 
-	service { "postgres": ensure => running }
+	service { "postgresql": ensure => running }
 
 	user { "ddf":
 		ensure => 'present'
 	}
 
-	exec { "get_ddf":
-		cwd => "/tmp",
-		command => "wget https://dl.dropbox.com/u/1627760/package-${version}.zip -O ddf.zip",
-		creates => "/tmp/ddf.zip",
-		timeout => 600,
+	# Get the DDF distribution
+	file { "set_wgetrc":
+		path => "/root/.wgetrc",
+		source => "puppet:///modules/ddf/wgetrc",
+		owner => "root",
+		group => "root"
 	}
 
+	exec { "get_ddf":
+		cwd => "/tmp",
+		command => "wget https://visualsvn.macefusion.com/svn/DDF/file_releases/ddf/${version}/package-${package}-${version}.zip -O ddf.zip --no-check-certificate",
+		creates => "/tmp/ddf.zip",
+		timeout => 600,
+		require => File["set_wgetrc"]
+	}
+
+	exec { "rm /root/.wgetrc":
+		require => Exec["get_ddf"]
+	}
+
+	# Unpack the DDF distribution
 	exec { "unzip /tmp/ddf.zip":
 		cwd => "/usr/local",
-		creates => "/usr/local/ddf-${version}",
+		creates => "/usr/local/ddf-${package}-${version}",
 		require => [Package["unzip"], Exec["get_ddf"], User['ddf']],
 	}
 
-	file { "/usr/local/ddf-${version}":
+	file { "/usr/local/ddf-${package}-${version}":
 		owner => "ddf",
 		group => "ddf",
 		recurse => true,
 		require => Exec["unzip /tmp/ddf.zip"],
 	}
 
+	# Setup the system service
 	file { "/etc/init.d/ddf":
 		source => "puppet:///modules/ddf/ddf",
-		require => File["/usr/local/ddf-${version}"],
+		require => File["/usr/local/ddf-${package}-${version}"],
 		mode => 755
 	}
-	file { "/usr/local/ddf-${version}/lib/libwrapper.so":
+	file { "/usr/local/ddf-${package}-${version}/lib/libwrapper.so":
 		source => "puppet:///modules/ddf/libwrapper.so",
-		require => File["/usr/local/ddf-${version}"],
+		require => File["/usr/local/ddf-${package}-${version}"],
 		mode => 644
 	}
-	file { "/usr/local/ddf-${version}/lib/karaf-wrapper.jar":
+	file { "/usr/local/ddf-${package}-${version}/lib/karaf-wrapper.jar":
 		source => "puppet:///modules/ddf/karaf-wrapper.jar",
-		require => File["/usr/local/ddf-${version}"],
+		require => File["/usr/local/ddf-${package}-${version}"],
 		mode => 644
 	}
-	file { "/usr/local/ddf-${version}/lib/karaf-wrapper-main.jar":
+	file { "/usr/local/ddf-${package}-${version}/lib/karaf-wrapper-main.jar":
 		source => "puppet:///modules/ddf/karaf-wrapper-main.jar",
-		require => File["/usr/local/ddf-${version}"],
+		require => File["/usr/local/ddf-${package}-${version}"],
 		mode => 644
 	}  
-	file { "/usr/local/ddf-${version}/bin/DDF-wrapper":
+	file { "/usr/local/ddf-${package}-${version}/bin/DDF-wrapper":
 		source => "puppet:///modules/ddf/DDF-wrapper",
 		mode => 755,
 	} 
-	file { "/usr/local/ddf-${version}/etc/DDF-wrapper.conf":
+	file { "/usr/local/ddf-${package}-${version}/etc/DDF-wrapper.conf":
 		mode => 644,
 		content => template("ddf/DDF-wrapper.conf.erb")
 	}
@@ -65,8 +82,8 @@ class ddf($version = "enterprise-2.1.0.ALPHA3") {
 		hasstatus => true,
 		hasrestart => true,
 		require => [File["/etc/init.d/ddf"],
-					File["/usr/local/ddf-${version}/bin/DDF-wrapper"],
-					File["/usr/local/ddf-${version}/etc/DDF-wrapper.conf"],]
+					File["/usr/local/ddf-${package}-${version}/bin/DDF-wrapper"],
+					File["/usr/local/ddf-${package}-${version}/etc/DDF-wrapper.conf"],]
 	}
 
 
