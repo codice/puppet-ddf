3761b085a18c1c4d592a623fd59c10a8c975b749
Kit Plummer <kitplummer@gmail.com>
2013-03-01 08:51:41 -0700

    more bug fixin

--- manifests/init.pp
+++ manifests/init.pp
@@ -57,7 +57,8 @@ class ddf($package = "ddf-standard",
 		
 	if $package == 'ddf-enterprise' {
 	# Unpack the DDF distribution
-		exec { "unzip /tmp/${version}/${package}-${version}.zip":
+		exec { "unzip_enterprise":
+      command => "unzip /tmp/${version}/${package}-${version}.zip",
 			cwd => "/usr/local",
 			creates => "/usr/local/${package}-${version}",
 			require => [Package["unzip"], Exec["get_ddf"], User['ddf']],
@@ -68,9 +69,13 @@ class ddf($package = "ddf-standard",
 			cwd => "/usr/local",
 			creates => "/usr/local/${package}-${version}",
 			require => [Package["unzip"], Exec["get_ddf"],  User['ddf']],
+      notify => File["/usr/local/${package}-${version}"]
 		}
-	}
-
+  }
+  file { "/usr/local/${package}-${version}":
+    ensure => directory,
+  }
+  
 	# Setup the system service
 	file { "/etc/init.d/ddf":
 		notify => Service["ddf"],
@@ -80,15 +85,18 @@ class ddf($package = "ddf-standard",
 	}
 	file { "/usr/local/${package}-${version}/lib/libwrapper.so":
 		source => "puppet:///modules/ddf/libwrapper.so",
-		mode => 644
+		mode => 644,
+    require => File["/usr/local/${package}-${version}"]
 	}
 	file { "/usr/local/${package}-${version}/lib/karaf-wrapper.jar":
 		source => "puppet:///modules/ddf/karaf-wrapper.jar",
-		mode => 644
+		mode => 644,
+    require => File["/usr/local/${package}-${version}"]
 	}
 	file { "/usr/local/${package}-${version}/lib/karaf-wrapper-main.jar":
 		source => "puppet:///modules/ddf/karaf-wrapper-main.jar",
-		mode => 644
+		mode => 644,
+    require => File["/usr/local/${package}-${version}"]
 	}
 
 	# Setup the appropriate wrapper
@@ -96,21 +104,24 @@ class ddf($package = "ddf-standard",
 		file { "/usr/local/${package}-${version}/bin/DDF-wrapper":
 			source => "puppet:///modules/ddf/DDF-wrapper",
 			mode => 755,
+      require => File["/usr/local/${package}-${version}"]
 		} 
 	} else {
 		file { "/usr/local/${package}-${version}/bin/DDF-wrapper":
 			source => "puppet:///modules/ddf/DDF-wrapper-32",
 			mode => 755,
+      require => File["/usr/local/${package}-${version}"]
 		} 
 	}
 	file { "/usr/local/${package}-${version}/etc/DDF-wrapper.conf":
 		mode => 644,
 		content => template("ddf/DDF-wrapper.conf.erb"),
-		require => File["/etc/init.d/ddf"]
+		require => [File["/etc/init.d/ddf"],File["/usr/local/${package}-${version}"]]
 	}
 	file { "/usr/local/${package}-${version}/etc/startup.properties":
 		mode => 644,
 		source => "puppet:///modules/ddf/startup.properties",
+    require => File["/usr/local/${package}-${version}"]
 	}
 
 }
