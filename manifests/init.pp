99885799bc37942646542900e5aa6ab649579800
Kit Plummer <kitplummer@gmail.com>
2013-06-25 09:08:04 -0700

    run_as fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,8 @@ class ddf($package = "ddf-standard",
   $java_home = "/usr/local/java",
   $mvn_repos = [],
   $feature_repos = [],
-  $features = []  
+  $features = [],
+  $ddf_user = "ddf"  
 ){
 
   if !defined(Service["iptables"]) {
@@ -17,25 +18,32 @@ class ddf($package = "ddf-standard",
     hasstatus   => true,
     hasrestart  => true,
     subscribe   => [File["/usr/local/${package}-${version}/etc/DDF-wrapper.conf"],
+      File["/etc/init.d/ddf"],
+      File["/usr/local/${package}-${version}"],
       File["/usr/local/${package}-${version}/etc/org.ops4j.pax.url.mvn.cfg"],
+      File["/usr/local/${package}-${version}/lib/karaf-wrapper.jar"],
+      File["/usr/local/${package}-${version}/lib/karaf-wrapper-main.jar"],
       File["/usr/local/${package}-${version}/etc/org.apache.karaf.features.cfg"]],
-    require     => [  File["/usr/local/${package}-${version}/bin/DDF-wrapper"],
+    require     => [ File["/usr/local/${package}-${version}"],
+        File["/usr/local/${package}-${version}/bin/DDF-wrapper"],
         File["/usr/local/${package}-${version}/etc/DDF-wrapper.conf"],
         File["/etc/init.d/ddf"],
         File["/usr/local/${package}-${version}/lib/libwrapper.so"],
         File["/usr/local/${package}-${version}/lib/karaf-wrapper.jar"],
         File["/usr/local/${package}-${version}/lib/karaf-wrapper-main.jar"],
         File["/usr/local/${package}-${version}/etc/org.ops4j.pax.url.mvn.cfg"],
-        File["/usr/local/${package}-${version}/etc/org.apache.karaf.features.cfg"]    ]
+        File["/usr/local/${package}-${version}/etc/org.apache.karaf.features.cfg"],
+        User[$ddf_user]]
   }
 
   # Ensure system dependencies are installed
-  user { "ddf":
-    ensure => 'present'
+  user { $ddf_user:
+    ensure      => 'present',
+    managehome  => true
   }
-    package{ "unzip": 
-        ensure => 'installed' 
-      } ->
+  package{ "unzip": 
+    ensure => 'installed' 
+  } ->
   exec { "get_ddf":
     cwd     => "/tmp",
     command => "/usr/bin/wget http://artifacts.codice.org/content/repositories/releases/ddf/distribution/${package}/${version}/${package}-${version}.zip --no-check-certificate",
@@ -60,64 +68,76 @@ class ddf($package = "ddf-standard",
       notify  => File["/usr/local/${package}-${version}"]
     }
   }
-  
-  file { "/usr/local/${package}-${version}":
-    ensure => directory
-  }
-  
+      
   # Setup the system service
   file { "/etc/init.d/ddf":
     notify  => Service["ddf"],
     content => template("ddf/ddf.erb"),
     require => File["/usr/local/${package}-${version}/etc/startup.properties"],
-    mode    => "0755",
+    mode    => "0755"
   }
   file { "/usr/local/${package}-${version}/lib/libwrapper.so":
     source  => "puppet:///modules/ddf/libwrapper.so",
     mode    => "0644",
+    group   => $ddf_user,
     require => File["/usr/local/${package}-${version}"]
   }
   file { "/usr/local/${package}-${version}/lib/karaf-wrapper.jar":
     source  => "puppet:///modules/ddf/karaf-wrapper.jar",
     mode    => "0644",
+    group   => $ddf_user,
     require => File["/usr/local/${package}-${version}"]
   }
   file { "/usr/local/${package}-${version}/lib/karaf-wrapper-main.jar":
     source  => "puppet:///modules/ddf/karaf-wrapper-main.jar",
     mode    => "0644",
+    group   => $ddf_user,
     require => File["/usr/local/${package}-${version}"]
   }
   file { "/usr/local/${package}-${version}/etc/org.ops4j.pax.url.mvn.cfg":
     content => template("ddf/org.ops4j.pax.url.mvn.cfg.erb"),
-    mode    => "0644",                              
+    mode    => "0644",
+    group   => $ddf_user                              
   }
   file { "/usr/local/${package}-${version}/etc/org.apache.karaf.features.cfg":
     content => template("ddf/org.apache.karaf.features.cfg.erb"),
-    mode    => "0644",                              
+    mode    => "0644",
+    group   => $ddf_user                              
   }
   # Setup the appropriate wrapper
   if $::architecture == 'x86_64' {  
     file { "/usr/local/${package}-${version}/bin/DDF-wrapper":
       source  => "puppet:///modules/ddf/DDF-wrapper",
       mode    => "0755",
+      group   => $ddf_user,
       require => File["/usr/local/${package}-${version}"]
     } 
   } else {
     file { "/usr/local/${package}-${version}/bin/DDF-wrapper":
       source  => "puppet:///modules/ddf/DDF-wrapper-32",
       mode    => "0755",
+      group   => $ddf_user,
       require => File["/usr/local/${package}-${version}"]
     } 
   }
   file { "/usr/local/${package}-${version}/etc/DDF-wrapper.conf":
     mode    => "0644",
+    group   => $ddf_user,
     content => template("ddf/DDF-wrapper.conf.erb"),
     require => [File["/etc/init.d/ddf"],File["/usr/local/${package}-${version}"]]
   }
   file { "/usr/local/${package}-${version}/etc/startup.properties":
     mode    => "0644",
+    group   => $ddf_user,
     source  => "puppet:///modules/ddf/startup.properties",
     require => File["/usr/local/${package}-${version}"]
+  }
+  file { "/usr/local/${package}-${version}":
+    ensure  => "directory",
+    group   => $ddf_user,
+    owner   => $ddf_user,
+    recurse => true,
+                                
   }
 
 }
